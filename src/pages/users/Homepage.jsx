e7534696e10339bf20f37f0eed0d112d5ba53029
ashutosh verma <ashutoshverma5555@gmail.com>
2021-10-31 12:08:35 +0530

    completed home apge

--- src/pages/users/Homepage.jsx
+++ src/pages/users/Homepage.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import '../assets/styles/homepage.css'
-import shoppingImg from '../assets/img/shopping-img.svg'
+import React from "react";
+import "../assets/styles/homepage.css";
+import shoppingImg from "../assets/img/shopping-img.svg";
 function Home() {
-    return (
-        <>
-               <div className="ball1"></div>
-               <div className="ball2"></div>
-           <div className="container">
-
-               <div className="home container-box">
-                 <img src={shoppingImg} alt="" />
-                 <div className="intro">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi maxime dignissimos earum recusandae blanditiis rerum sint? Ex iure quisquam sunt cum numquam nam nobis accusantium ullam necessitatibus consequatur. Molestiae eius atque esse eos vero totam similique ut, dolorum doloribus fuga libero! Cupiditate pariatur hic facere?</div>
-               </div>
-              
-           </div>
-        </>
-    )
+  return (
+    <>
+      <div className="ball1"></div>
+      <div className="ball2"></div>
+      <div className="container">
+        <div className="home container-box">
+          <img src={shoppingImg} alt="" />
+          <div className="intro">
+            <span className="style-text">
+              Hey are you missing out a great deal just because you don’t have a
+              right credit or a debit card???
+            </span>{" "}
+            <br /> Just Relax….. Here is a solution - REBATE <br /> Rebate is a
+            place where you can get additional discounts on your purchase👛💸
+            Interesting right!
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
