acc9763804d5b2f3b286776c1ec09a0f87885d37
ashutosh verma <ashutoshverma5555@gmail.com>
2021-10-31 14:05:04 +0530

    update in home page

--- src/pages/users/Homepage.jsx
+++ src/pages/users/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../assets/styles/homepage.css";
 import shoppingImg from "../assets/img/shopping-img.svg";
+import homepageImg2 from '../assets/img/home-page-img-2.svg'
 function Home() {
   return (
     <>
       <div className="ball1"></div>
       <div className="ball2"></div>
       <div className="container">
+        <h1 className="main-heading"><span className="style-heading">R</span>ebate</h1>
         <div className="home container-box">
           <img src={shoppingImg} alt="" />
           <div className="intro">
@@ -14,9 +16,14 @@ function Home() {
               Hey👋!!! <br /> Are you missing out a great deal just because you
               don’t have a right credit or a debit card???
             </span>
-            <br /> Just Relax….. <br /> Here is a solution - REBATE <br /> Rebate is a
-            place where you can get additional discounts on your purchase👛💸
-            with secure payments... <br />
+            <br /> Just Relax….. <br /> Here is a solution - REBATE <br />
+          </div>
+        </div>
+        <div className="home1 container-box">
+          <img src={homepageImg2} alt="" />
+          <div className="intro1">
+            Rebate is a place where you can get additional discounts on your
+            purchase👛💸 with secure payments... <br />
             Interesting right !!!!!
           </div>
         </div>
